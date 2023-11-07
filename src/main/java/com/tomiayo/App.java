be0c8d42f2f5b5e1c012810146825338adb1f3b9
Tobi bjt <callbj@gmail.com>
2023-11-07 22:35:47 +0000

    commiting first files

---
+++ src/main/java/com/tomiayo/App.java
@@ -0,0 +1,13 @@
+package com.tomiayo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
